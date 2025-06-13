85a07ed80849219b5dc86a427594f908e1a2b7e9
FunGrape <foxclub237@gmail.com>
2025-06-13 14:51:53 +0300

    Maven виправлення

--- src/com/javalessons/lesson11/maven/src/main/java/org/example/Main.java
+++ src/com/javalessons/lesson11/maven/src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        org.example.JsonWorker worker = new org.example.JsonWorker();
+    public static void main(String[] args){
+        JsonWorker worker = new JsonWorker();
         String json = worker.toJson();
         String studentJson = worker.toObject(json);
 
